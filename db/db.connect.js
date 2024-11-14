5aaa8ddff1e3605dea4a1d42a0a354dc407addb2
Omkar Parmar <omkarparmar04@hotmail.com>
2024-11-14 12:04:56 +0530

    feat: change in dbconnection

--- db/db.connect.js
+++ db/db.connect.js
@@ -1,10 +1,21 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
+
 const mongoUri = process.env.MONGODB;
 
-const initializDatabase = async() => {
-    await mongoose.connect(mongoUri).then(() => {
-        console.log("Connected to Database")
-    }).catch((error) => console.log("Error connect to Database", error));
-}
-module.exports = {initializDatabase};
+const initializDatabase = async () => {
+    try {
+        await mongoose.connect(mongoUri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true, // If using older versions of Mongoose (<6.0)
+            useFindAndModify: false // If using older versions of Mongoose (<6.0)
+        });
+        console.log("Connected to Database");
+    } catch (error) {
+        console.error("Error connecting to Database:", error.message);
+        process.exit(1); // Exit the process if the database connection fails
+    }
+};
+
+module.exports = { initializDatabase };
