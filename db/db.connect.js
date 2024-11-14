1b925a41cf0701381b9ad9bc4bbc38e8498c6bd1
Omkar Parmar <omkarparmar04@hotmail.com>
2024-11-14 12:47:34 +0530

    fix: change in dbconnection

--- db/db.connect.js
+++ db/db.connect.js
@@ -1,15 +1,18 @@
-require("dotenv").config(); // Load environment variables at the top
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+require('dotenv').config();  // Ensure environment variables are loaded
 
-const mongoUri = process.env.MONGODB; // Ensure this line captures the URI from .env
+const mongoUri = process.env.MONGODB;
 
 const initializDatabase = async () => {
+    if (!mongoUri) {
+        console.error("MongoDB URI not found. Please check your .env file.");
+        process.exit(1); // Exit the process if the URI is not found
+    }
+
     try {
         await mongoose.connect(mongoUri, {
             useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModify: false
+            useUnifiedTopology: true
         });
         console.log("Connected to Database");
     } catch (error) {
