f02f56470952ff6bdff0bd7b3f7a4d9453cb84d7
Omkar Parmar <omkarparmar04@hotmail.com>
2024-11-14 12:17:33 +0530

    feat: adding some more changes in dbconnection

--- db/db.connect.js
+++ db/db.connect.js
@@ -8,12 +8,10 @@ const initializDatabase = async () => {
         await mongoose.connect(mongoUri, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useCreateIndex: true, // If using older versions of Mongoose (<6.0)
-            useFindAndModify: false // If using older versions of Mongoose (<6.0)
         });
         console.log("Connected to Database");
     } catch (error) {
-        console.error("Error connecting to Database:", error.message);
+        console.error("Error connecting to Database:", error);
         process.exit(1); // Exit the process if the database connection fails
     }
 };
